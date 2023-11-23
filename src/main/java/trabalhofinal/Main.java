8638a80aa92d0cd371f2de4bd37ef9a3ef497453
Gabriel Paredes <gabriel.paredes@estudante.iftm.edu.br>
2023-11-23 10:39:29 -0300

    Jorge

--- src/main/java/trabalhofinal/Main.java
+++ src/main/java/trabalhofinal/Main.java
@@ -2,6 +2,7 @@ package trabalhofinal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("JOrge");
+        System.out.println("JOrge2");
     }
 }
