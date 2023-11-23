39b5add0372dfc1a57e754e5f2b7b577096e1d19
Gabriel Paredes <gabriel.paredes@estudante.iftm.edu.br>
2023-11-23 12:08:43 -0300

    Main modificado inicial

--- src/main/java/trabalhofinal/Main.java
+++ src/main/java/trabalhofinal/Main.java
@@ -1,8 +1,33 @@
 package trabalhofinal;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("JOrge");
-        System.out.println("JOrge2");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application{
+
+    public static Scene scene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        scene = new Scene(loadFXML("telaprincial"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
